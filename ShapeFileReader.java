42a6140f01c3dd573f7eb34f72aa671a8a05412b
nozbi <76044507+nozbi@users.noreply.github.com>
2023-08-27 22:51:17 +0200

    Add files via upload

--- ShapeFileReader.java
+++ ShapeFileReader.java
@@ -1,60 +1,118 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.swing.JFileChooser;
 import javax.swing.JPanel;
-import javax.swing.plaf.FileChooserUI;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 
 public class ShapeFileReader 
 {
+    private boolean fileCorrect;
+    private Vertex[] vertices;
+    private Wall[] walls;
+
     public ShapeFileReader(JPanel panelParameter)
     {
         JFileChooser fileChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
+        fileChooser.setFileFilter(filter);
         File selectedFile = null;
-        fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
-        int result = fileChooser.showOpenDialog(panelParameter);
-        if (result == JFileChooser.APPROVE_OPTION) 
+        fileChooser.showOpenDialog(panelParameter);
+        selectedFile = fileChooser.getSelectedFile();
+        if (selectedFile != null) 
         {
-            selectedFile = fileChooser.getSelectedFile();
-        } 
-        else 
-        {
-            
+            try
+            {
+                ArrayList<String> verticesStrings = new ArrayList<String>();
+                ArrayList<String> wallsStrings = new ArrayList<String>();
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(selectedFile));
+                String line;
+                boolean verticesLoaded = false;
+                while ((line = bufferedReader.readLine()) != null) 
+                {
+                    if(verticesLoaded == false)
+                    {
+                        if(line.isEmpty())
+                        {
+                            verticesLoaded = true;
+                        }
+                        else
+                        {
+                            verticesStrings.add(line);
+                        }
+                    }
+                    else
+                    {
+                        if(line.isEmpty() == false)
+                        {
+                            wallsStrings.add(line);
+                        }
+                    }
+                }
+                bufferedReader.close();
+
+                this.vertices = this.convertVerticesStringsToVertices(verticesStrings);
+                this.walls = this.convertWallsStringsToWalls(wallsStrings);
+                this.fileCorrect = true;
+            }
+            catch(Exception exception){}
         }
     }
 
     public Vertex[] getVertices()
     {
-        Vertex[] vertices = 
-        {
-           new Vertex(-100, 100, 100),
-           new Vertex(100, 100, 100),
-           new Vertex(100, -100, 100),
-           new Vertex(-100, -100, 100),
-           new Vertex(-100, 100, -100),
-           new Vertex(100, 100, -100),
-           new Vertex(100, -100, -100),
-           new Vertex(-100, -100, -100)
-        };
-
-        return vertices;
+        return this.vertices;
     }
 
     public Wall[] getWalls()
     {
-        Wall[] walls =
-        {
-            new Wall(new int[]{0, 1, 2, 3}),
-            new Wall(new int[]{4, 5, 6, 7}),
-            new Wall(new int[]{0, 3, 7, 4}),
-            new Wall(new int[]{1, 2, 6, 5}),
-            new Wall(new int[]{2, 3, 7, 6}),
-            new Wall(new int[]{0, 4, 5, 1})
-        };
-
-        return walls;
+        return this.walls;
     }
 
     public boolean isFileGood()
     {
-        return true;
+        return this.fileCorrect;
+    }
+
+    private Vertex[] convertVerticesStringsToVertices(ArrayList<String> verticesStringsParameter)
+    {
+        Vertex[] convertedVertices = new Vertex[verticesStringsParameter.size()];
+        for (int i = 0; i < verticesStringsParameter.size(); i++) 
+        {
+            convertedVertices[i] = this.convertVertexStringToVertex(verticesStringsParameter.get(i));
+        }
+        return convertedVertices;
+    }
+
+    private Wall[] convertWallsStringsToWalls(ArrayList<String> wallsStringsParameter)
+    {
+        Wall[] convertedWalls = new Wall[wallsStringsParameter.size()];
+        for (int i = 0; i < wallsStringsParameter.size(); i++) 
+        {
+            convertedWalls[i] = this.convertWallStringToWall(wallsStringsParameter.get(i));
+        }
+        return convertedWalls;
+    }
+
+    private Vertex convertVertexStringToVertex(String vertexStringParameter)
+    {
+        List<String> vertexStrings = Arrays.asList(vertexStringParameter.split(","));
+        return new Vertex(Double.parseDouble(vertexStrings.get(0)), Double.parseDouble(vertexStrings.get(1)), Double.parseDouble(vertexStrings.get(2)));
+    }
+
+    private Wall convertWallStringToWall(String wallStringParameter)
+    { 
+        List<String> wallStrings = Arrays.asList(wallStringParameter.split(","));
+        int[] wallVerticesIndexes = new int[wallStrings.size()];
+        for(int i = 0; i < wallStrings.size(); i++)
+        {
+            wallVerticesIndexes[i] = Integer.parseInt(wallStrings.get(i));
+        }
+        return new Wall(wallVerticesIndexes);
     }
 }
